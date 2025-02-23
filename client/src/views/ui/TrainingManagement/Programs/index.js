d278501a62bcdf71b4d6c55459d95baedd672389
Moebu <benchmanuel2002@gmail.com>
2025-02-23 16:54:28 +0800

    add button form training management

--- client/src/views/ui/TrainingManagement/Programs/index.js
+++ client/src/views/ui/TrainingManagement/Programs/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {
+    CFormTextarea,
     CAccordion,
     CAccordionItem,
     CAccordionHeader,
@@ -27,7 +28,7 @@ import {
 } from '@coreui/react'
 
 const TrainingDelivery = () => {
-    const [visibleXL, setVisibleXL] = useState(false)
+    const [visibleProgram, setVisibleProgram] = useState(false)
     const [visibleTargetAudience, setVisibleTargetAudience] = useState(false)
     const [visibleCourse, setVisibleCourse] = useState(false)
 
@@ -36,9 +37,9 @@ const TrainingDelivery = () => {
             <CCol xs={12}>
                 <CCard className="mb-4">
                     <CCardHeader>
-                        <strong>Training Program Details</strong>
+                        <strong>List of Program</strong>
                         <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <CButton color="primary" onClick={() => setVisibleXL(true)}>
+                            <CButton color="primary" onClick={() => setVisibleProgram(true)}>
                                 Add Program
                             </CButton>
                         </div>
@@ -54,7 +55,7 @@ const TrainingDelivery = () => {
                                                 <CTableHead>
                                                     <CTableRow>
                                                         <CTableHeaderCell>
-                                                            Description
+                                                            Program Description
                                                         </CTableHeaderCell>
                                                     </CTableRow>
                                                 </CTableHead>
@@ -122,13 +123,12 @@ const TrainingDelivery = () => {
                                         <CAccordionItem>
                                             <CAccordionHeader>Courses</CAccordionHeader>
                                             <CAccordionBody>
+                                                <strong>List of Courses</strong>
                                                 <div className="d-grid gap-2 d-md-flex justify-content-md-end mb-2">
                                                     <CButton
                                                         color="primary"
                                                         size="sm"
-                                                        onClick={() =>
-                                                            setVisibleTargetAudience(true)
-                                                        }
+                                                        onClick={() => setVisibleCourse(true)}
                                                     >
                                                         Add Course
                                                     </CButton>
@@ -198,6 +198,116 @@ const TrainingDelivery = () => {
                     </CCardBody>
                 </CCard>
             </CCol>
+
+            {/*ADD PROGRAM FORM*/}
+
+            <CModal size="xl" visible={visibleProgram} onClose={() => setVisibleProgram(false)}>
+                <CModalHeader onClose={() => setVisibleProgram(false)}>
+                    <CModalTitle>Add Program</CModalTitle>
+                </CModalHeader>
+                <CModalBody>
+                    <CForm>
+                        <CRow className="mb-3">
+                            <CCol md={12}>
+                                <CFormLabel htmlFor="programTitle">Program Title</CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    id="programTitle"
+                                    placeholder="Enter program title"
+                                />
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CCol md={12}>
+                                <CFormLabel htmlFor="programDescription">
+                                    Program Description
+                                </CFormLabel>
+                                <CFormTextarea
+                                    id="programDescription"
+                                    rows="3"
+                                    placeholder="Enter program description"
+                                />
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CCol md={12}>
+                                <CFormLabel htmlFor="programGoal">Program Goal</CFormLabel>
+                                <CFormTextarea
+                                    id="programGoal"
+                                    rows="3"
+                                    placeholder="Enter program goal"
+                                />
+                            </CCol>
+                        </CRow>
+                    </CForm>
+                </CModalBody>
+                <CModalFooter>
+                    <CButton color="secondary" onClick={() => setVisibleProgram(false)}>
+                        Close
+                    </CButton>
+                    <CButton color="primary">Save Program</CButton>
+                </CModalFooter>
+            </CModal>
+
+            {/*ADD COURSE FORM*/}
+
+            <CModal size="xl" visible={visibleCourse} onClose={() => setVisibleCourse(false)}>
+                <CModalHeader onClose={() => setVisibleCourse(false)}>
+                    <CModalTitle>Add Course</CModalTitle>
+                </CModalHeader>
+                <CModalBody>
+                    <CForm>
+                        <CRow className="mb-3">
+                            <CCol md={12}>
+                                <CFormLabel htmlFor="programTitle">Course Title</CFormLabel>
+                                <CFormInput
+                                    type="text"
+                                    id="programTitle"
+                                    placeholder="Enter course title"
+                                />
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CCol md={12}>
+                                <CFormLabel htmlFor="programDescription">
+                                    Course Description
+                                </CFormLabel>
+                                <CFormTextarea
+                                    id="programDescription"
+                                    rows="3"
+                                    placeholder="Enter course description"
+                                />
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CCol md={12}>
+                                <CFormLabel htmlFor="programGoal">Course Goal</CFormLabel>
+                                <CFormTextarea
+                                    id="programGoal"
+                                    rows="3"
+                                    placeholder="Enter course goal"
+                                />
+                            </CCol>
+                        </CRow>
+                        <CRow className="mb-3">
+                            <CCol md={12}>
+                                <CFormLabel htmlFor="programGoal">Course Goal</CFormLabel>
+                                <CFormTextarea
+                                    id="programGoal"
+                                    rows="3"
+                                    placeholder="Enter course goal"
+                                />
+                            </CCol>
+                        </CRow>
+                    </CForm>
+                </CModalBody>
+                <CModalFooter>
+                    <CButton color="secondary" onClick={() => setVisibleCourse(false)}>
+                        Close
+                    </CButton>
+                    <CButton color="primary">Save Program</CButton>
+                </CModalFooter>
+            </CModal>
         </CRow>
     )
 }
