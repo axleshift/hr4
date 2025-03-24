a9d5ffbca8228f27c202f929b6382a923561567a
Moebu <benchmanuel2002@gmail.com>
2025-03-24 13:25:58 +0800

    lint error

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,24 +1,27 @@
-module.exports = {
-    // parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
-        ecmaFeatures: {
-            jsx: true, // Allows for the parsing of JSX
+module.exports = [
+  {
+    languageOptions: {
+      ecmaVersion: 2020, // Allows for modern ECMAScript features
+      sourceType: "module", // Allows for import/export
+      parser: require("@babel/eslint-parser"), // Use Babel parser for JSX
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
         },
+      },
+    },
+    plugins: {
+      react: require("eslint-plugin-react"),
+      "react-hooks": require("eslint-plugin-react-hooks"),
     },
     settings: {
-        react: {
-            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-        },
+      react: {
+        version: "detect", // Detects the React version automatically
+      },
     },
-    extends: [
-        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    ],
-    plugins: ['react', 'react-hooks'],
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+      ...require("eslint-plugin-react").configs.recommended.rules,
     },
-}
+  },
+];
