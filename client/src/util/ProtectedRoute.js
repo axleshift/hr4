d0d3a96ed8ad63285f2507e86d8f3cfeb1f077bb
Moebu <benchmanuel2002@gmail.com>
2025-03-28 17:20:02 +0800

    fix hook=hooks

--- client/src/util/ProtectedRoute.js
+++ client/src/util/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
-import useAuthStatus from '../hook/useAuthStatus'
+import useAuthStatus from '../hooks/useAuthStatus'
 
 const ProtectedRoute = () => {
     const status = useAuthStatus()
