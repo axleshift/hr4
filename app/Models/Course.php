0d33827b2ac701c2465f8ad054080eeb1d45e809
freight-capstone <mrepol742@gmail.com>
2025-04-04 22:47:42 +0000

    Production

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,16 +9,16 @@ class Course extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'program_id'];
-
+    // Define the fillable attributes for the course model
+    protected $fillable = [
+        'title',          // Course title
+        'description',    // Course description
+        'program_id',     // Foreign key to the Program
+        'file_path',      // Path to the uploaded file
+        'file_name',      // The file's name
+    ];
     public function program()
     {
-        return $this->belongsTo(Program::class);
+        return $this->belongsTo(Program::class);  // Course belongs to a Program
     }
-    public function modules()
-    {
-        return $this->hasMany(Module::class);
-    }
-
 }
-
