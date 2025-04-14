81405e3d3961dc75adf0bdf1c5eab14254899212
freight-capstone <mrepol742@gmail.com>
2025-04-14 05:43:18 +0000

    Production

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,6 +29,10 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     public function role(): BelongsTo
     {
         return $this->belongsTo(Role::class);
