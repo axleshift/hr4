0d1915c123e9ab17b528023871edf7de58e49cd1
Moebu <benchmanuel2002@gmail.com>
2025-04-16 13:25:50 +0800

    remove routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\Api\ModuleController;
 use App\Http\Controllers\Api\FileController;
 use App\Http\Controllers\Api\ProgramController;
 use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/roles', [RoleController::class, 'index']);
 
 Route::get('/', function () {
     return response()->json(['message' => 'Hello, World!']);
@@ -36,7 +39,3 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\UserProfileController;
 
 Route::get('/users', [AuthController::class, 'index']);
-
-Route::get('/users', [UserProfileController::class, 'index']);
-Route::get('/users/{id}', [UserProfileController::class, 'show']);
-Route::put('/users/{id}', [UserProfileController::class, 'update']);
