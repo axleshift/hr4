27b9e6875dd3db10edb70e2ac293a76651bcdd3f
freight-capstone <mrepol742@gmail.com>
2025-04-17 03:45:29 +0000

    Production

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
                     'id' => $user->id,
                     'name' => $user->name,
                     'email' => $user->email,
-                    'role' => $user->role->name ?? 'No Role',
+                    'role' => $user->role->name,
                 ];
             }),
         ]);
