fb62a7025ff8d730fe2a382fb422163e7f826919
Moebu <benchmanuel2002@gmail.com>
2025-04-22 23:40:26 +0800

    PROFILE revert

--- client/src/views/ui/EmployeeManagement/Profile/index.js
+++ client/src/views/ui/EmployeeManagement/Profile/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { useSelector } from 'react-redux'
+import PropTypes from 'prop-types' // Import PropTypes
 import {
     CModal,
     CModalBody,
@@ -12,30 +11,28 @@ import {
     CFormLabel,
     CFormInput,
 } from '@coreui/react'
+import api from '../../../../util/api'
 
 const ProfileModal = ({ modalVisible, setModalVisible }) => {
-    const user = useSelector((state) => state.auth.user)
-
     return (
         <CModal visible={modalVisible} onClose={() => setModalVisible(false)}>
             <CModalHeader>
                 <CModalTitle>Profile</CModalTitle>
             </CModalHeader>
             <CModalBody>
-                {user ? (
-                    <CForm>
-                        <CFormLabel htmlFor="fullName">Full Name</CFormLabel>
-                        <CFormInput id="fullName" value={user.name} readOnly />
+                <CForm>
+                    <CFormLabel htmlFor="fullName">Full Name</CFormLabel>
+                    <CFormInput id="fullName" value="John Doe" readOnly />
 
-                        <CFormLabel htmlFor="email">Email</CFormLabel>
-                        <CFormInput id="email" value={user.email} readOnly />
+                    <CFormLabel htmlFor="email">Email</CFormLabel>
+                    <CFormInput id="email" value="johndoe@example.com" readOnly />
 
-                        <CFormLabel htmlFor="role">Role</CFormLabel>
-                        <CFormInput id="role" value={user.role} readOnly />
-                    </CForm>
-                ) : (
-                    <p>Loading profile...</p>
-                )}
+                    <CFormLabel htmlFor="role">Role</CFormLabel>
+                    <CFormInput id="role" value="Software Engineer" readOnly />
+
+                    <CFormLabel htmlFor="department">Department</CFormLabel>
+                    <CFormInput id="department" value="Engineering" readOnly />
+                </CForm>
             </CModalBody>
             <CModalFooter>
                 <CButton color="secondary" onClick={() => setModalVisible(false)}>
@@ -46,6 +43,7 @@ const ProfileModal = ({ modalVisible, setModalVisible }) => {
     )
 }
 
+// Add PropTypes validation
 ProfileModal.propTypes = {
     modalVisible: PropTypes.bool.isRequired,
     setModalVisible: PropTypes.func.isRequired,
