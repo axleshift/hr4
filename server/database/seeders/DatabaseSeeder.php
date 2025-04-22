7c1771e595f0cfe26d7af06cc7253e4b7909e327
Moebu <benchmanuel2002@gmail.com>
2025-04-22 22:24:00 +0800

    remove hash dbseed

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -8,7 +8,6 @@ use App\Models\Role;
 use App\Models\Program;
 use App\Models\Course;
 use App\Models\Training;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,60 +19,63 @@ class DatabaseSeeder extends Seeder
         $staffRole = Role::firstOrCreate(['name' => 'staff']);
         $employeeRole = Role::firstOrCreate(['name' => 'employee']);
 
-        // Users
+        // Create a Super Admin
         User::factory()->create([
             'name' => 'Super Admin',
             'email' => 'sadmin@gmail.com',
-            'password' => Hash::make('superadmin123'),
-            'role_id' => $superAdminRole->id,
+            'password' => bcrypt('superadmin123'), // Secure password
+            'role_id' => $superAdminRole->id, // Assign role
         ]);
 
+        // Create an Admin
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('admin123'),
-            'role_id' => $adminRole->id,
+            'password' => bcrypt('admin123'), // Secure password
+            'role_id' => $adminRole->id, // Assign role
         ]);
 
+        // Create 2 Staff members
         User::factory()->create([
             'name' => 'Manager',
             'email' => 'staff1@gmail.com',
-            'password' => Hash::make('staff123'),
+            'password' => bcrypt('staff123'),
             'role_id' => $staffRole->id,
         ]);
 
         User::factory()->create([
             'name' => 'Trainer',
             'email' => 'staff2@gmail.com',
-            'password' => Hash::make('staff123'),
+            'password' => bcrypt('staff123'),
             'role_id' => $staffRole->id,
         ]);
 
+        // Create 3 Employees
         User::factory()->create([
             'name' => 'Cristy',
             'email' => 'employee1@gmail.com',
-            'password' => Hash::make('employee123'),
+            'password' => bcrypt('employee123'),
             'role_id' => $employeeRole->id,
         ]);
 
         User::factory()->create([
             'name' => 'Malabad',
             'email' => 'employee2@gmail.com',
-            'password' => Hash::make('employee123'),
+            'password' => bcrypt('employee123'),
             'role_id' => $employeeRole->id,
         ]);
 
         User::factory()->create([
             'name' => 'Guma',
             'email' => 'employee3@gmail.com',
-            'password' => Hash::make('employee123'),
+            'password' => bcrypt('employee123'),
             'role_id' => $employeeRole->id,
         ]);
 
         User::factory()->create([
             'name' => 'Tidoso',
             'email' => 'employee4@gmail.com',
-            'password' => Hash::make('employee123'),
+            'password' => bcrypt('employee123'),
             'role_id' => $employeeRole->id,
         ]);
 
