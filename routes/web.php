18ab7ac6dfb878eaf329a367f5cc3aff39df9ae4
freight-capstone <mrepol742@gmail.com>
2025-04-28 14:11:51 +0000

    Production

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,5 +13,7 @@ Route::prefix('api/auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/verify-session', [AuthController::class, 'verifySession']);
 });
+
+Route::get('auth/google', [GoogleAuthController::class, 'redirect'])->name('google-auth');
 
 Route::fallback(fn() => view('index'));
