e88551ecc67e635e1f59d7843490af8b48a06d9b
Moebu <benchmanuel2002@gmail.com>
2025-05-01 16:28:54 +0800

    department AC tweak3

--- client/src/views/pages/login/Login.js
+++ client/src/views/pages/login/Login.js
@@ -54,6 +54,7 @@ const Login = () => {
             sessionStorage.setItem('user_id', user.id)
             sessionStorage.setItem('name', user.name)
             sessionStorage.setItem('email', user.email)
+            sessionStorage.setItem('department', user.departmemt)
 
             navigate('/dashboard')
         } catch (err) {
@@ -85,7 +86,6 @@ const Login = () => {
             })
 
             sessionStorage.setItem('session_id', session_id)
-            sessionStorage.setItem('role', user.role)
             sessionStorage.setItem('user_id', user.id)
             sessionStorage.setItem('name', user.name)
             sessionStorage.setItem('email', user.email)
