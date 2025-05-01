8ce5413f829206005e4f22c5f04d58b74b14e6ab
Moebu <benchmanuel2002@gmail.com>
2025-05-01 13:23:50 +0800

    department AC update

--- client/src/views/ui/EmployeeManagement/AccessControl/index.js
+++ client/src/views/ui/EmployeeManagement/AccessControl/index.js
@@ -150,7 +150,7 @@ const AccessControl = () => {
                                                 {user.role}
                                             </CTableDataCell>
                                             <CTableDataCell className="text-center">
-                                                {user.departm1ent ? user.department.name : 'N/A'}
+                                                {user.departm1ent}
                                             </CTableDataCell>
                                             <CTableDataCell className="text-center">
                                                 <CButton
