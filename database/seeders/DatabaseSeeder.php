c654200170b852764f89c75c9dabda6f6cee4cec
freight-capstone <mrepol742@gmail.com>
2025-05-01 09:01:02 +0000

    Production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,11 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Role;
+use App\Models\Department;
 use App\Models\Program;
 use App\Models\Course;
 use App\Models\Training;
-use App\Models\Department;
-
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
