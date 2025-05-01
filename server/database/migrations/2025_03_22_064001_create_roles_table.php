3ee442e705946b2d222f1d712b12b25a0df3fc44
Moebu <benchmanuel2002@gmail.com>
2025-05-01 17:16:02 +0800

    dep database tweak

--- server/database/migrations/2025_03_22_064001_create_roles_table.php
+++ server/database/migrations/2025_03_22_064001_create_roles_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
         });
 
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->after('id')->nullable()->constrained('roles')->onDelete('set null');
+            $table->foreignId('role_id')->nullable()->after('id')->nullable()->constrained('roles')->onDelete('set null');
         });
     }
 
