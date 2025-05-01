27f34d7f43e57dcae052f87baf69c42a812579d4
freight-capstone <mrepol742@gmail.com>
2025-05-01 23:32:26 +0000

    Production

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\EmployeeResource;
 
 class EmployeeController extends Controller
