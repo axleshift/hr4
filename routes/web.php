98852c46a9f09a5f48162e9b291355fb38bbed58
freight-capstone <mrepol742@gmail.com>
2025-05-01 03:40:16 +0000

    Production

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ Route::prefix('api/auth')->group(function () {
     Route::post('/verify-session', [AuthController::class, 'verifySession']);
     Route::get('auth/google', [GoogleAuthController::class, 'redirect'])->name('google-auth');
     Route::get('auth/google/call-back', [GoogleAuthController::class, 'callbackGoogle']);
+    Route::post('auth/google', [GoogleAuthController::class, 'callbackGoogle']);
+    Route::post('auth/google/call-back', [GoogleAuthController::class, 'callbackGoogle']);
 });
 
 
