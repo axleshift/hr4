f2f0446773f617e5971f98288d948a7d3b1317b4
Moebu <benchmanuel2002@gmail.com>
2025-05-02 10:26:08 +0800

    test department auth

--- client/src/views/ui/EmployeeManagement/AccessControl/index.js
+++ client/src/views/ui/EmployeeManagement/AccessControl/index.js
@@ -150,7 +150,7 @@ const AccessControl = () => {
                                                 {user.role}
                                             </CTableDataCell>
                                             <CTableDataCell className="text-center">
-                                                {user.department}
+                                                {user.departments}
                                             </CTableDataCell>
                                             <CTableDataCell className="text-center">
                                                 <CButton
