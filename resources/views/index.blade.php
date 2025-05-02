fa0e3ed64bfbcc13e5e70c9f02c5ba6c1239c849
freight-capstone <mrepol742@gmail.com>
2025-05-02 03:43:49 +0000

    Production

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
     <title>Freight Main</title>
     <link rel="manifest" href="/manifest.json">
     <link rel="shortcut icon" href="/favicon.ico">
-    <script type="module" crossorigin src="/assets/index-DNzQ3P0i.js"></script>
+    <script type="module" crossorigin src="/assets/index-B_3uHjii.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/index-DQb7sx6g.css">
   </head>
   <body>
